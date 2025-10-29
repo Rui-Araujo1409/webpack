b0056fd14980ee6776473ebf54e0f2565398bcfb
Rui-Araujo1409 <rui1409@gmail.com>
2025-10-29 11:12:31 +0000

    final teste webpack

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,9 +1,15 @@
 import _ from "lodash";
+import "./style.css";
+import teorba from "./teorba.jpg";
 
 function component() {
     const element = document.createElement("div");
 
     element.innerHTML = _.join(["Boas a todos", "webpack"], " ");
+    element.classList.add("boas");
+    const teorbaHTML = new Image();
+    teorbaHTML.src = teorba;
+
 
     return element;
 }
